3a03c9a200a1b164c20d10e0a3368a456fe68980
kglibrarian <karen.gutzman@northwestern.edu>
2019-03-09 15:50:42 -0600

    worked on pypoll code

--- PyPoll/pypoll.py
+++ PyPoll/pypoll.py
@@ -1,39 +1,3 @@
-#The total number of votes cast
-#A complete list of candidates who received votes
-#The percentage of votes each candidate won
-#The total number of votes each candidate won
-#The winner of the election based on popular vote.
-
-def voterInfo(voterID, county, candidate):
-    #print(voterID)
-    
-    #The total number of votes cast
-    voterSum = len(voterID)
-    print(voterSum)
-    
-    #A complete list of candidates who received votes
-    candidateUnique = set(candidate)
-    print(candidateUnique)
-
-    #The percentage of votes each candidate won
-    #for i in candidate: 
-        #my_dict = candidate.count(i) 
-        #print(my_dict)
-    candidateSum = {i:candidate.count(i) for i in candidate}
-    print(candidateSum)
-
-    for i in candidateSum: 
-        candidateAvg = ((candidateSum[i]/voterSum)*100)
-        print(candidateAvg)
-    
-    
-
-
-
-    #The total number of votes each candidate won
-    #The winner of the election based on popular vote.
-    result = list(zip(voterID, county, candidate))
-    #print(result)
 # Import the os module to create file paths across operating systems
 import os
 
@@ -41,6 +5,106 @@ import os
 import csv
 
 csvpath = os.path.join('election_data_test.csv')
+
+#```text
+  #Election Results
+  #-------------------------
+  #Total Votes: 3521001
+  #-------------------------
+  #Khan: 63.000% (2218231)
+  #Correy: 20.000% (704200)
+  #Li: 14.000% (492940)
+  #O'Tooley: 3.000% (105630)
+  #-------------------------
+  #Winner: Khan
+ # -------------------------
+  #```
+
+#The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote.
+
+def voterInfo(voterID, county, candidate):
+        
+    #The total number of votes cast
+    voterSum = len(voterID)
+    print(f"Total Votes: {voterSum}")
+   
+    #A complete list of candidates who received votes
+    #candidateUnique = set(candidate)
+    #print(candidateUnique)
+
+    #Create dicitonary of Key: candidate name and Value: number of votes
+    candidateSum = {i:candidate.count(i) for i in candidate}
+    
+    #print("candidateSum: ", candidateSum)
+    for i in candidateSum:
+        print(i)
+    
+    #candidatePercent = []
+
+    #create an empty dictionary for the final data
+    finalData = {}
+
+    #loop through each Key: candidate in our current dictionary
+    for k, v in candidateSum.items():
+        if v < 
+        #print(k, v[0], v[1])
+        print(f"{k} : {v[0]} ({v[1]})")
+        
+        #each Key: candidate should be i
+        #print("i: ", i)
+        
+        #sum the number of unique times each candidate was voted for
+        candidateVoteSum = len(i)
+        #print("CandidateVoteSum: ", candidateVoteSum)
+        
+        #get the average number of votes each candidate received - create a percentage and round to two decimals
+        candidateAvg = (candidateSum[i]/voterSum)*100
+        candidateAvgRound = round(candidateAvg,3)
+        #print("CandidateAvgRound: ",  candidateAvgRound)
+
+        #create a list of the data for each candidate - which is the average number of votes received, and the number of votes
+        candidateInfo = {i:[candidateAvgRound, candidateVoteSum]}
+        #print("CandidateInfo: ", candidateInfo)
+        
+        #add this new informaiton into our dictionary
+        finalData.update(candidateInfo)
+    
+    #print the finalData dictionary
+    #print(finalData)
+    
+    #loop through the items in the dictionary and print them
+    for k, v in finalData.items():
+        #print(k, v[0], v[1])
+        print(f"{k} : {v[0]} ({v[1]})")
+           
+    #print(finalData(1))
+        #candidatePercent.append(candidateInfo)
+    #print(candidatePercent)
+    #print(candidatePercent.iteritems())
+    
+    #delete from function here down!!
+
+    #print(f"{candidatePercent[0]} \n" f"{candidatePercent[1]}")
+    #print(f"Average Change: ${round(average,2)}" )
+
+        #candidatePercent.append({i:candidateAvg})
+      
+    
+    #result = list(zip(candidateUnique, candidateAvg))
+    #print(result)
+
+
+    #The total number of votes each candidate won
+    #The winner of the election based on popular vote.
+    #result = list(zip(voterID, county, candidate))
+    #print(result)
+
+
+
 
 with open(csvpath, newline='') as csvfile:
 
@@ -51,7 +115,7 @@ with open(csvpath, newline='') as csvfile:
 
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
     voterID = []
     county = []
